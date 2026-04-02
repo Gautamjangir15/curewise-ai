8048f04e03b14c30b9ea5687fe9ff4b2b9949bb9
Gautamjangir15 <gautamjangir2002@gmail.com>
2026-04-03 00:30:08 +0530

    Initial commit - Curewise AI

--- advisor.py
+++ advisor.py
@@ -5,9 +5,6 @@ from config import GROQ_API_KEY
 
 client = Groq(api_key=GROQ_API_KEY)
 
-
-#client = Groq(api_key=os.getenv("GROQ_API_KEY"))
-
 def generate_medical_advice(symptoms, diseases, age, gender, bmi, allergies, severe_issues):
 
     advice_dict = {}
